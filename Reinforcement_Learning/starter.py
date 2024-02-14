cdc59aab0aed5006ec5155f9d4aea1b4cde52333
Yize Chen <yizechen@uw.edu>
2024-02-14 17:08:53 -0500

    Update starter.py

--- Reinforcement_Learning/starter.py
+++ Reinforcement_Learning/starter.py
@@ -5,3 +5,5 @@ bandit_model = sagemaker.model.Model(
     model_data=f"s3://{s3_bucket}/{model_path}",
     sagemaker_session=sage_session,
 )
+
+#Tutorials from https://github.com/aws/amazon-sagemaker-examples/blob/main/reinforcement_learning/bandits_recsys_movielens_testbed/bandits_movielens_testbed.ipynb
